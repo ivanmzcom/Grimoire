7830e4094ff55fdacda9c41dba1e04c442043962
Iván Moreno <ivan@ivanmz.com>
2026-04-23 08:57:21 +0200

    Initial Commit

---
+++ JuegosApp/JuegosAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  JuegosAppApp.swift
+//  JuegosApp
+//
+//  Created by Iván Moreno Zambudio on 23/4/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct JuegosAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
